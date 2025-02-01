6f06e234c525c9efedf9a0eea7c921e1d5efe095
rborgesc@nttdata.com <rborgesc@nttdata.com>
2025-02-01 13:45:33 -0300

    u

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from client.database import BigQuery
 
 st.logo('img/Passos-magicos-icon-cor-removebg-preview.png')
 
-page_introduction = st.Page('pages\introduction.py', title="Introdução", icon=":material/home:")
+page_introduction = st.Page('pages/introduction.py', title="Introdução", icon=":material/home:")
 page_report = st.Page('pages/report.py', title="Relatório", icon=":material/description:")
 page_dashboard = st.Page('pages/dashboard.py', title="Dashboard", icon=":material/analytics:")
 page_ml = st.Page('pages/ml_model.py', title="Machine Learning", icon=":material/smart_toy:")
