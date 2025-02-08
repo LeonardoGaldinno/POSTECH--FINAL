39d9749626890243ca285b1fdf744489beb843bc
rborgesc@nttdata.com <rborgesc@nttdata.com>
2025-02-08 11:35:39 -0300

    update

--- pages/report.py
+++ pages/report.py
@@ -65,6 +65,7 @@ st.markdown("""#### Análise da evolução das Classificações ao Longo dos Ano
 
 # st.pyplot(fig)
 # Filtrar alunos com classificação 3 em 2021
+# Filtrar alunos com classificação 3 em 2021
 alunos_classificacao_3_2021 = periododf[(periododf['ClassificacaoDescricao'] == 3) & (periododf['SiglaPeriodo'] == 2021)]
 
 # Obter os IDs dos alunos
@@ -91,7 +92,7 @@ chart = alt.Chart(evolucao_classificacao_long).mark_line(point=True).encode(
     height=400
 )
 
-# Exibir o gráfico no Streamlit
+# Exibir o gráfico no Jupyter Notebook
 st.altair_chart(chart)
 
 ### --------------- DF EVOLUÇÃO -----------------------------------###
