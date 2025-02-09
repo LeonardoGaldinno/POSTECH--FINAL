3d96db8f46cdaa64d42ae41ecd12d850ed7d84e8
rborgesc@nttdata.com <rborgesc@nttdata.com>
2025-02-09 19:30:44 -0300

    teste

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -20,5 +20,7 @@ year_range = st.slider("Selecione o período:", 2019, 2024,(2020,2022))
 genres = ["Action", "Adventure", "Biography", "Comedy", "Drama", "Horror"]
 selected_genres = st.multiselect("Selecione os gêneros:", genres, default=["Action", "Comedy"])
 
+st.write(year_range)
+
 
 st.write(f"Analisando de {year_range[0]} até {year_range[1]} para os gêneros: {', '.join(selected_genres)}.")
