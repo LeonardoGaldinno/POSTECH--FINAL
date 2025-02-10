1d8299d1406dd09fe5fb204fc4ad910ef985ee8f
rborgesc@nttdata.com <rborgesc@nttdata.com>
2025-02-09 21:48:52 -0300

    fixing bugs

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -16,7 +16,7 @@ col1, col2 = st.columns(2)
 with col1:
 
     selected_classification = []
-    st.markdown("""#### Selecione a classificação do aluno""")
+    st.write("Selecione a classificação do aluno")
     good = st.checkbox("1 - Desempenho Bom ")
     regular = st.checkbox("2 - Desempenho Regular")
     bad = st.checkbox("3 - Desempenho Inferior")
@@ -40,7 +40,7 @@ with col1:
 with col2:
     
     schools = []
-    st.markdown("""#### Selecione a Instituição de Ensino""")
+    st.write("Selecione a Instituição de Ensino")
     private_school = st.checkbox("Escolas Particulares")
     public_school = st.checkbox("Escolas Públicas")
 
