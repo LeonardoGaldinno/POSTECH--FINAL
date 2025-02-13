20518be7dd5e68770cb789f973c1f8bced47132c
rborgesc@nttdata.com <rborgesc@nttdata.com>
2025-02-13 18:56:38 -0300

    test model

--- pages/ml_model.py
+++ pages/ml_model.py
@@ -20,6 +20,7 @@ ida = st.number_input("Indicador de desenvolvimento acadêmico", value=0.0)
 if st.button("Prever pedra"):
     
     valores = np.array([[ieg, ipv, ida]])
+    st.write(valores)
     predicao = modelo.predict(valores)
 
     st.write(predicao)
